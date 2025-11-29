ad1cbb6e80e658084b04262d886f6698717eb122
Moinul Islam Raj <moinulislamraj0@gmail.com>
2025-11-29 14:22:55 +0600

    Re-design

--- data.js
+++ data.js
@@ -4,29 +4,59 @@ export const projects = [
         details: "Connect with others in a chat room. You can create your own account and chat with any other user anytime. The UI is responsive for all kinds of devices.",
         imagesrc: "",
         projectlink: "https://theraj0.github.io/messages",
+        buttontext: "Visit",
     },
     {
         name: "Organic Reaction App",
         details: "This web application can help you find the reactions needed to convert one organic compound into any other organic compound. The design is simple and elegant made for simple use.",
         imagesrc: "",
         projectlink: "https://theraj0.github.io/organic_chemistry",
+        buttontext: "Visit",
     },
     {
         name: "Meme Generator",
         details: "This is a web app that extracts images from a meme api and you can type over those images in the app itself to create new memes. But to download those memes you have to screenshot it.",
         imagesrc: "",
         projectlink: "https://theraj0.github.io/meme-generator",
+        buttontext: "Visit",
     },
     {
         name: "Javascript Animations",
         details: "This is a side project that is specifically designed for PC users not mobile users. It has many of the javascript canvas animation project that I made.",
         imagesrc: "",
         projectlink: "https://theraj0.github.io/javascript_animations",
+        buttontext: "Visit",
     },
     {
         name: "ChemConvert",
         details: "My mobile application version of Organic Reaction Web app. You can download the apk below.",
         imagesrc: "",
         projectlink: "https://drive.google.com/file/d/1e_0tnVlfHdvmn48XGZypkuwNYrJPl6oo/view?usp=drive_link",
+        buttontext: "Download",
+    }
+]
+
+export const skills = [
+    {
+        title: "Pentesting & Security",
+        text: `Reconnaissance with ~~Nmap~, packet analysis with ~~Wireshark~, 
+                        web testing via ~~Burp Suite~, password attacks using ~~Hydra~ & ~~Hashcat~, 
+                        experience with ~~Metasploit Framework~,
+                        hands-on experience with various ~~Kali Linux~ tools.`,
+    },
+    {
+        title: "Programming",
+        text: `~~Python~ for scripting and automation, ~~Arduino/C~ for hardware projects, 
+                        ~~C++~ and ~~JavaScript~ (~~React~, ~~Node.js~) for apps and small projects.`,
+    },
+    {
+        title: "Operating Systems & Dev Tools",
+        text:  `Daily use of ~~Debian~/~~Kali Linux~, system recovery, package management, 
+                        troubleshooting, with basic experience in ~~Docker~, ~~Git~, and offline ~~Android APK~ creation.`,
+    },
+    {
+        title: "Creative & Multimedia Tools",
+        text: `~~Unity~ (game development), ~~Blender~ (3D modeling), 
+                        ~~GIMP~ (image editing), ~~Kdenlive~ (video editing), and small web applications.`,
     }
 ]
--- index.js
+++ index.js
@@ -1,26 +1,55 @@
-import { projects } from "./data.js"
+import { projects, skills } from "./data.js"
 
-function createProject(name, bio, image, link) {
+function highlighthtml(text) {
+    text = text.replaceAll("~~", `<span class="highlight">`)
+    text = text.replaceAll("~", `</span>`)
+
+    return text
+}
+
+function createProject(name, bio, image, link, btntxt) {
     let output = (`
     <div class="project">
         <h3>` + name +`</h3>
         <p>` + bio +`</p>
         
         <form target="_blank" action="` + link + `">
-            <button>Go</button>
+            <button>` + btntxt + `</button>
         </form>
     </div>
     `)
     return output
 }
 
-const containerEl = document.getElementById("projectscontainer")
+function createSkill(title, text) {
+
+    let formattedtext = highlighthtml(text);
+
+    let output = (`
+    <div class="skill-card">
+        <h3>` + title + `</h3>
+        <p>` + formattedtext + `</p>
+    </div>    
+    `)
+    return output
+}
+
+const projContainerEl = document.getElementById("projectscontainer")
+const skillsContainerEl = document.getElementById("skillscontainer")
 
 window.onload = () => {
-    let projstr = "";
+    let projstr = ""
     projects.forEach(p => {
-        projstr += createProject(p.name, p.details, p.imagesrc, p.projectlink)
+        projstr += createProject(p.name, p.details, p.imagesrc, p.projectlink, p.buttontext)
     })
 
-    containerEl.innerHTML = projstr
+    projContainerEl.innerHTML = projstr
+
+    let skillstr = ""
+    skills.forEach(s => {
+        skillstr += createSkill(s.title, s.text)
+    })
+
+    skillsContainerEl.innerHTML = skillstr
+
 }
